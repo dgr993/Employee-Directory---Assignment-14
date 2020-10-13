19436c96a52fef30c93e459d72075a7350f78276
Wuzup <dgr993@gmail.com>
2020-10-13 16:34:26 -0600

    added a filter table and tweaking it to show the data

--- src/components/ResultList.js
+++ src/components/ResultList.js
@@ -1,14 +1,40 @@
-import React from "react";
+import React, {useState} from "react";
+import FilterTable from 'react-filter-tables';
 
 function ResultList(props) {
+ const [res,setres] = useState ({})
+ 
+ {
+ props.results.map(result => {
+   let data = [
+     {
+       'Id': result.id,
+       'type': 'test',
+       'name': result.name.first,
+       'username': 'abcd123'
+     }
+   ]
+  setres(data);
+ 
+ })}
+  console.log(res);
   return (
-    <ul className="list-group">
-      {props.results.map(result => (
-        <li className="list-group-item" key={result.id}>
-          <img alt={result.title} className="img-fluid" src={result.images.original.url} />
-        </li>
+    <>
+    {/* <FilterTable data={data}/> */}
+    
+      
+       {props.results.map(result => (
+        <div>
+        <p className="list-group-item" key={result.id}>
+          <img alt={result.title} className="img-fluid" src={result.picture.thumbnail} />
+        </p>
+         <p className="list-group-item" key={result.id}> </p>
+         <p>{result.name.first} {result.name.last}</p>
+       
+       </div>
       ))}
-    </ul>
+    
+    </>
   );
 }
 

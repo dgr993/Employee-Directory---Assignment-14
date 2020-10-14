88281c3322fdf155dba72499d77fc3ddafa7f121
Wuzup <dgr993@gmail.com>
2020-10-13 21:49:52 -0600

    started search bar

---
+++ components/SearchResultContainer.js
@@ -0,0 +1,66 @@
+import React, { Component } from "react";
+import SearchForm from "./SearchForm";
+import ResultList from "./ResultList";
+import API from "../../utils/API";
+
+class SearchResultContainer extends Component {
+  state = {
+    search: "",
+    results: []
+  };
+
+  // When this component mounts, search the Giphy API for pictures of kittens
+  componentDidMount() {
+    this.searchGiphy();
+  }
+
+  searchGiphy = () => {
+    API.search()
+      .then(res => 
+      this.setState({ results: res.data.results })
+       )
+      .catch(err => console.log(err));
+  };
+
+  handleInputChange = event => {
+    const name = event.target.name;
+    const value = event.target.value;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleSearchChange = event => {
+    console.log(event.target.value);
+    const filter = event.target.value;
+    const filteredList = this.state.users.filter(item => {
+      // merge data together, then see if user input is anywhere inside
+      let values = Object.values(item)
+        .join("")
+        .toLowerCase();
+      return values.indexOf(filter.toLowerCase()) !== -1;
+    });
+    this.setState({ filteredUsers: filteredList });
+  }
+
+  // When the form is submitted, search the Giphy API for `this.state.search`
+  handleFormSubmit = event => {
+    event.preventDefault();
+    this.searchGiphy(this.state.search);
+  };
+
+  render() {
+    return (
+      <div>
+        <SearchForm
+          search={this.state.search}
+          handleFormSubmit={this.handleFormSubmit}
+          handleInputChange={this.handleInputChange}
+        />
+        <ResultList results={this.state.results} />
+      </div>
+    );
+  }
+}
+
+export default SearchResultContainer;

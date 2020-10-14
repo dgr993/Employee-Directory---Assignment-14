4857ea7297c8a86ae8bcf9d22e4d4e60d14a66a2
Wuzup <dgr993@gmail.com>
2020-10-13 21:57:13 -0600

    tried to deploy to github

---
+++ build/precache-manifest.78b7923f0c05a73006cfac2c4662fd48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b85f31b828a4f55ac7c5ca798df5081",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2566c563d0ed62cc06d",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "c2566c563d0ed62cc06d",
+    "url": "/static/js/2.6e6a578e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6e6a578e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0529745084e373268ffa",
+    "url": "/static/js/main.12aa13a6.chunk.js"
+  },
+  {
+    "revision": "5b446e10af4fafd5a747",
+    "url": "/static/js/runtime-main.f71b05c2.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import SearchResultContainer from "./components/SearchResultContainer";
+import { HashRouter, Route, Link } from "react-router-dom";
 
 function App() {
   return <SearchResultContainer />;
 }
+
 
 export default App;

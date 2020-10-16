39cb8e5a08ba6618db076a964054ae36224ce8a5
Wuzup <dgr993@gmail.com>
2020-10-15 21:48:44 -0600

    Updates

---
+++ precache-manifest.6be967269a40dbcc4fef91b61c67e6a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76023a72ee4cc9d5c29e015f41bf1e4b",
+    "url": "/Employee-Directory---Assignment-14/index.html"
+  },
+  {
+    "revision": "c2566c563d0ed62cc06d",
+    "url": "/Employee-Directory---Assignment-14/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "c2566c563d0ed62cc06d",
+    "url": "/Employee-Directory---Assignment-14/static/js/2.6e6a578e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Employee-Directory---Assignment-14/static/js/2.6e6a578e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0529745084e373268ffa",
+    "url": "/Employee-Directory---Assignment-14/static/js/main.12aa13a6.chunk.js"
+  },
+  {
+    "revision": "87a8f4ea1f5ac9853926",
+    "url": "/Employee-Directory---Assignment-14/static/js/runtime-main.9f16a6b3.js"
+  }
+]);
